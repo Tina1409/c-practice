05dc27a7ce9626291e7b72153029d963aae97de7
Tina <tinav1409@gmail.com>
2022-10-08 22:25:36 +0530

    BestPrice

---
+++ bestPrice.c
@@ -0,0 +1,24 @@
+//best price
+#include<stdio.h>
+int deparment(){
+	
+}
+int main(){
+	int choice;
+	printf("\t\t------Welcome To Best Price------\n\n");
+	printf("\t------Select the department you want to go------\n");
+	int selectDepartment=department();
+	if(selectDeparmtent==?){
+		//function
+	}
+	else if(selectDepartment==?){
+		//function
+	}
+	else if(selectDepartment==?){
+		//function
+	}
+	else if(selectDepartment==?){
+		//function
+	}
+}
+
