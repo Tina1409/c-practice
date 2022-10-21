edf52e1430b510bfba7ef19af3209f85737eb8f0
Tina <tinav1409@gmail.com>
2022-10-21 16:42:49 +0530

    NoOfDigitsInANumberUsingFunctions

--- howManyDigitsInaNumberUsingFunctions.c
+++ howManyDigitsInaNumberUsingFunctions.c
@@ -1,1 +1,18 @@
 //how many digits in a number using function
+#include<stdio.h>
+int main(){
+	countDigits();
+}
+
+int countDigits(){
+	int n;
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	int count =0;
+	while(n!=0){
+		n=n/10;
+		count++;
+	}
+	printf("the no of digits are:%d",count);
+}
+
