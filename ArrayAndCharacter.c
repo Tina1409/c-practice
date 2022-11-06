5cd1244af19d5bd9420fa060a4744d17f82e35ef
Tina <tinav1409@gmail.com>
2022-11-06 21:02:10 +0530

    ArrayAndCharacter

---
+++ ArrayAndCharacter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	//printing an array
+	int a[10];
+	printf("Enter a string ");
+	gets(a);
+	printf("%s\n",a);
+	
+	//printing a character
+	int s;
+	printf("Enter a character: ");
+	scanf("%c",&s);
+	printf("%c",s);
+}
